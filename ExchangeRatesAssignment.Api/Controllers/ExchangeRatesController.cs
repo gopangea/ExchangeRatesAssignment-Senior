3e0d97e0655ac54ea4abe36fb5ba87b2d4c9c444
Keerthi Maryala <keerthi@gopangea.com>
2024-03-14 17:19:08 -0500

    Skeleton

---
+++ ExchangeRatesAssignment.Api/Controllers/ExchangeRatesController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using ExchangeRatesAssignment.Api.Contracts;
+
+namespace ExchangeRatesAssignment.Api.Controllers
+{
+    [ApiController]
+    public class ExchangeRatesController : ControllerBase
+    {
+        // TODO : Implement an API endpoint that takes a three letter country code and returns a collection of
+        // calculated exchange rates of the type Api.Contracts.PangeaExchangeRate
+
+        
+    }
+}
